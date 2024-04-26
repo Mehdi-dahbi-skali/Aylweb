c16f6cfd5645028dc489a0cda6eadaa9fb86d7b1
Ordinateur <mds.skali@gmail.com>
2024-04-26 23:53:59 +0100

    test

--- src/Pages/Sections/Dashboard.js
+++ src/Pages/Sections/Dashboard.js
@@ -7,7 +7,7 @@ const Dashboard = () => {
         <div className="">
             <Nav />
             <div className="h-[100vh] w-full flex flex-col justify-center text-center bg-cover    " style={{ backgroundImage: `url(${back})`, backgroundPosition: 'center right' }}>
-                <h1 className='font-Lostar text-[131.05px] text-white  mt-[13rem]' >AYL AGENCYY</h1>
+                <h1 className='font-Lostar text-[131.05px] text-white  mt-[13rem]' >AYL AGENCYYYY</h1>
                 <p className=' font-poppins text-[15px] text-white mt-[-2rem] ' >Purpose-Driven growth</p>
                 <div className=" flex justify-center items-center p-5 ">
                     <a href="/#Services" className=' font-poppins  text-[20.39px] text-[#FFF500]  ' >Services</a>
